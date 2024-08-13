6633c268683452a68a6a4c792b81803f170bfb82
Kozhevnikov Dmitriy <kozhevnikov.dmitriy@sonakai.com>
2024-08-13 10:19:02 +0400

    Start project

---
+++ math-calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        private void OutPut()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
+
