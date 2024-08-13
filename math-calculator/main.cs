fb2abe5b25b5c5e4c44214a6ff271097d879f293
Kozhevnikov Dmitriy <kozhevnikov.dmitriy@sonakai.com>
2024-08-13 16:13:35 +0400

    - create start menu

--- math-calculator/main.cs
+++ math-calculator/main.cs
@@ -1,22 +1,81 @@
 ﻿using System;
 
-namespace MathCalculator
+namespace math_calculator
 {
-    internal class Program
+    public class Program
     {
-        private static void Main()
-        {
-            var text = "";
+        const int delayBeforeExit = 800;
 
-            text = Console.ReadLine();
-            OutPut(text);
+        private static void MenuMainMathCalculator()
+        {
+            bool IsExit = false;
+            var numOfMenu = "0";
+
+            while (!IsExit)
+            {
+                Console.Clear();
+                Console.Write("MAIN MENU\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nChoose:\n1) Standart calculator\n0) Exit\nEnter:");
+                numOfMenu = Console.ReadLine();
+                Console.Clear();
+
+                switch (numOfMenu)
+                {
+                    case "1":
+                        MenuFirstStandartCalculator();
+                        break;
+                    case "0":
+                        IsExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("You enter not correct nuber of menu!\nPlease, try again.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+            Console.WriteLine("EXIT.");
+            Thread.Sleep(delayBeforeExit);
+            Console.Clear();
+            Console.WriteLine("EXIT..");
+            Thread.Sleep(delayBeforeExit);
+            Console.Clear();
+            Console.WriteLine("EXIT...");
+            Thread.Sleep(delayBeforeExit);
         }
 
-        private static void OutPut(string textForOutPut)
+        private static void MenuFirstStandartCalculator()
         {
-            //Test pull request!
-            Console.WriteLine("U write: " + textForOutPut);
-            Console.ReadKey();
+            bool IsExit = false;
+            var numOfMenu = "0";
+
+            while (!IsExit)
+            {
+                Console.Clear();
+                Console.Write("STANDART CALCULATOR\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nChoose:\n1) Enter values\n0) Back\nEnter:");
+                numOfMenu = Console.ReadLine();
+                Console.Clear();
+
+                switch (numOfMenu)
+                {
+                    case "1":
+                        Console.WriteLine("...Start algorizm...\n");
+                        //StandartCalculator.AlgorithmStandartCalculator();
+                        break;
+                    case "0":
+                        IsExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("You enter not correct nuber of menu!\nPlease, try again.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        //I don't undestand. WHY the main method of c# procject must be static?
+        private static void Main(string[] args)
+        {
+            MenuMainMathCalculator();
         }
     }
 }
